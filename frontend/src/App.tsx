ba64d732aa86f2978b3101f7a9bca7113e6caa42
DAR0987 <darshanshrestha619@gmail.com>
2026-08-02 16:14:12 +0545

    Marvel Heroes Classification Starts (first commit)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <h1 className='text-5xl font-medium text-[#807f7f]'>Vite React</h1> 
+    </div>
+  ) 
+}
+
+export default App
