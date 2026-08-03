e2b56f5fb2c70b3e5ccc4f4a4a5d0f1d4167cf2c
DAR0987 <darshanshrestha619@gmail.com>
2026-08-03 13:50:14 +0545

    Added: Notebook added where the experiments of the Marvel Heroes Prediction Project was conducted and the model was trained

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
 
   return (
-    <div className='h-screen flex justify-center items-center'>
-      <h1 className='text-5xl font-medium text-[#807f7f]'>Vite React</h1> 
-    </div>
+    <>
+      
+    </>
   ) 
 }
 
