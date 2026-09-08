718052e9b52029d801fa07d2bb55dcdebbb2871e
DAR0987 <darshanshrestha619@gmail.com>
2026-09-08 13:59:39 +0545

    Added: Ipynb file where the model was trained in colab is added, trained keras model also included to be deployed in the backend itself as it's only 20mb

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -3,13 +3,17 @@ from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parents[2]
 
 
 class Settings(BaseSettings):
     APP_NAME: str = "Marvel Heroes Classifier API"
     APP_VERSION: str = "1.0.0"
-    DEBUG: bool = True
+
+    DEBUG: bool = False
+    ENVIRONMENT: str = "development"
+
+    FRONTEND_URL: str = "http://localhost:5173"
 
     MODEL_PATH: Path = (
         BASE_DIR / "assets" / "models" / "marvel_heroes_classifier_transfer_model.keras"
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from src.api.router import api_router
 from src.core.config import settings
 from src.core.lifespan import lifespan
-from src.api.router import api_router
 
 
 app = FastAPI(
@@ -13,16 +13,15 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:4173",
-    ],
+    allow_origins=[settings.FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(api_router)
 
